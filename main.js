fb95a1abd868109afa22b2c614f2b9b9158a394a
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2019-11-06 16:03:00 +0800

    Updated to only use 1 input

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 let input = document.getElementById("input");
 let output = document.getElementById("output");
-let eInput = document.getElementById("eInput");
-let eOutput = document.getElementById("eOutput");
 let ac = new AccountNumber()
 
 input.addEventListener("input", () => {
-    output.value = ac.decrypt(input.value);
-})
 
-// 1401020912
-
-eInput.addEventListener("input", () => {
-    eOutput.value = ac.encrypt(eInput.value);
+    // User inputted their name to retreive number (encode)
+    if(isNaN(input.value)){
+        output.value = ac.encrypt(input.value);
+        
+    }else{
+        output.value = ac.decrypt(input.value);
+    }
 })
